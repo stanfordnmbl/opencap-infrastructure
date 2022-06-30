8297690262ba6444cf9ff1be587835bccd932601
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2022-06-30 11:45:43 -0700

    At least 1 healthy machine

--- modules/processing/ecs_service.tf
+++ modules/processing/ecs_service.tf
@@ -3,5 +3,5 @@ resource "aws_ecs_service" "worker" {
   cluster         = aws_ecs_cluster.ecs_cluster.id
   task_definition = aws_ecs_task_definition.task_definition.arn
   desired_count   = var.num_machines
-  deployment_minimum_healthy_percent = 0
+  deployment_minimum_healthy_percent = 100
 }
