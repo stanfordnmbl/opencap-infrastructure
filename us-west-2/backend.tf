ebf512ee0a39ff51591939212e03c23e58936252
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2022-07-14 10:11:31 -0700

    Remove us-west-1

--- us-west-2/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "opencap-infra"
-    key    = "terraform-us-west-2"
-    region = "us-west-2"
-    profile = "mobilize"
-  }
-}
--- us-west-2/main.tf
+++
@@ -1,9 +0,0 @@
-module "processing" {
-    region = "us-west-2"
-    num_machines = 0
-    opencap_ecr_repository = aws_ecr_repository.opencap-opencap.repository_url
-    openpose_ecr_repository = aws_ecr_repository.opencap-openpose.repository_url
-    opencap_api_ecr_repository = aws_ecr_repository.opencap-api.repository_url
-    
-    source = "../modules/processing"
-}
