52c5ab1e3420d9fd609ca0893a4f18c269acb4bb
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2022-07-29 09:35:38 -0700

    Fargate upgrades

--- dev/main.tf
+++ dev/main.tf
@@ -3,9 +3,12 @@ module "processing" {
     num_machines = 0
     opencap_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/opencap"
     openpose_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/openpose"
-    opencap_api_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/api"
+    opencap_api_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/api-dev"
     env = "-dev"
     api_host = "dev.opencap.ai"
+    api_memory = 8192
+    api_cpu = 4096
+    api_servers = 1
     
     source = "../modules/processing"
 }
