1e50f53f3c04d4fb4f44faa9217d64b06e614d87
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2022-08-04 10:18:39 -0700

    Smaller server

--- dev/main.tf
+++ dev/main.tf
@@ -6,8 +6,8 @@ module "processing" {
     opencap_api_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/api-dev"
     env = "-dev"
     api_host = "dev.opencap.ai"
-    api_memory = 8192
-    api_cpu = 4096
+    api_memory = 2048
+    api_cpu = 1024
     api_servers = 1
     
     source = "../modules/processing"
