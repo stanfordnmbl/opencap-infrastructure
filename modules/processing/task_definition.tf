5f8722f879c0212f19fc3f499f805278d3b1e5f2
Oleh Korkh <korkholeh@gmail.com>
2023-02-22 16:31:11 +0200

    fixed indents

--- modules/processing/task_definition.tf
+++ modules/processing/task_definition.tf
@@ -12,8 +12,8 @@ resource "aws_ecs_task_definition" "task_definition" {
   family                = "worker${var.env}"
   container_definitions = data.template_file.task_definition_template.rendered
   execution_role_arn    = aws_iam_role.ecs_tasks_execution_role.arn
-  memory		= 7670
+  memory                = 7670
   volume {
-    name      = "data${var.env}"
+    name = "data${var.env}"
   }
 }
