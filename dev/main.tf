7342854c97676b1f530ebfec736dd7ed5a1eacfd
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2023-03-03 11:00:37 -0800

    Intel EC2 (rather than ARM) and mmpose repo path

--- dev/main.tf
+++ dev/main.tf
@@ -4,6 +4,7 @@ module "processing" {
     opencap_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/opencap"
     openpose_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/openpose"
     opencap_api_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/api-dev"
+    mmpose_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/mmpose"
     env = "-dev"
     api_host = "dev.opencap.ai"
     api_memory = 2048
--- modules/processing/elasticache.tf
+++ modules/processing/elasticache.tf
@@ -1,7 +1,7 @@
 resource "aws_elasticache_cluster" "redis_cache" {
   cluster_id           = "${var.app_name}-redis-cluster${var.env}"
   engine               = "redis"
-  node_type            = "cache.t4g.micro"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis3.2"
   engine_version       = "3.2.10"
