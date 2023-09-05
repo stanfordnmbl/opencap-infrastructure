b917af1aa0847297d1898d54bb1351c992326960
Antoine Falisse <antoinefalisse@gmail.com>
2023-09-05 10:35:27 -0700

    add gait analysis dev

--- dev/main.tf
+++ dev/main.tf
@@ -6,6 +6,7 @@ module "processing" {
     opencap_api_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/api-dev"
     mmpose_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap/mmpose"
     opencap_analysis_max_centerofmass_vpos_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap-analysis/max_centerofmass_vpos-dev"
+    opencap_gait_analysis_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap-analysis/gait_analysis-dev"
     env = "-dev"
     api_host = "dev.opencap.ai"
     api_memory = 2048
--- modules/processing/lambda.tf
+++ modules/processing/lambda.tf
@@ -3,6 +3,11 @@ variable "opencap_analysis_max_centerofmass_vpos_ecr_repository" {
   description = "Repository"
 }
 
+variable "opencap_gait_analysis_ecr_repository" {
+  type        = string
+  description = "Repository"
+}
+
 data "aws_secretsmanager_secret" "analysis_common_secrets" {
   arn = "arn:aws:secretsmanager:us-west-2:660440363484:secret:AnalysisFunctions${var.env}-TpGO1s"
 }
@@ -49,6 +54,45 @@ resource "aws_lambda_function_url" "analysis_max_centerofmass_vpos_url" {
 
 resource "aws_cloudwatch_log_group" "analysis_max_centerofmass_vpos_logs" {
   name              = "/aws/lambda/${aws_lambda_function.analysis_max_centerofmass_vpos.function_name}"
+  retention_in_days = 90
+}
+
+# Gait analysis
+
+resource "aws_lambda_function" "gait_analysis" {
+  function_name = "gait-analysis${var.env}"
+  timeout       = 900 # seconds
+  image_uri     = "${var.opencap_gait_analysis_ecr_repository}:latest"
+  package_type  = "Image"
+  memory_size = 1024
+
+  role = aws_iam_role.analysis_functions_execution_role.arn
+
+  environment {
+    variables = {
+      API_TOKEN = "${jsondecode(data.aws_secretsmanager_secret_version.analysis_common_secrets_version.secret_string)["api_token"]}"
+      API_URL = "${jsondecode(data.aws_secretsmanager_secret_version.analysis_common_secrets_version.secret_string)["api_url"]}"
+    }
+  }
+}
+
+
+resource "aws_lambda_function_url" "gait_analysis_url" {
+  function_name      = aws_lambda_function.gait_analysis.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
+
+resource "aws_cloudwatch_log_group" "gait_analysis_logs" {
+  name              = "/aws/lambda/${aws_lambda_function.gait_analysis.function_name}"
   retention_in_days = 90
 }
 
