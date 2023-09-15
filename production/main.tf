3988ad7660fca197fd1522ac7d21b111761a1f17
Antoine Falisse <antoinefalisse@gmail.com>
2023-09-15 15:14:32 -0700

    temporarily commenting out lambda stuff to make it run on prod

--- production/main.tf
+++ production/main.tf
@@ -5,7 +5,7 @@ module "processing" {
     openpose_ecr_repository = aws_ecr_repository.opencap-openpose.repository_url
     opencap_api_ecr_repository = aws_ecr_repository.opencap-api.repository_url
     mmpose_ecr_repository = aws_ecr_repository.opencap-mmpose.repository_url
-    opencap_analysis_max_centerofmass_vpos_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap-analysis/max_centerofmass_vpos"
+    # opencap_analysis_max_centerofmass_vpos_ecr_repository = "660440363484.dkr.ecr.us-west-2.amazonaws.com/opencap-analysis/max_centerofmass_vpos"
     
     source = "../modules/processing"
 }
