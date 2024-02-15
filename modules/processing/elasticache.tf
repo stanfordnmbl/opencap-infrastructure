397da5a46b0ce0f67d305e572dfdfad60462d7b9
Antoine Falisse <antoinefalisse@gmail.com>
2024-02-15 12:32:01 -0800

    upgrade redis

--- modules/processing/elasticache.tf
+++ modules/processing/elasticache.tf
@@ -3,8 +3,8 @@ resource "aws_elasticache_cluster" "redis_cache" {
   engine               = "redis"
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
+  parameter_group_name = "default.redis6.x"
+  engine_version       = "6.2"
   port                 = 6379
   subnet_group_name = aws_elasticache_subnet_group.redis_subnet.name
 }
