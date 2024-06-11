736be494becd3cfddcb4f46268eea4f309d03043
Antoine Falisse <antoinefalisse@gmail.com>
2024-06-11 10:10:25 -0700

    minor

--- modules/processing/ecs.tf
+++ modules/processing/ecs.tf
@@ -63,10 +63,6 @@ resource "aws_ecs_task_definition" "task_definition" {
   container_definitions = data.template_file.task_definition_template.rendered
   execution_role_arn    = aws_iam_role.ecs_tasks_execution_role.arn
   task_role_arn         = aws_iam_role.processing_worker_role.arn
-  # 31680(15840*2) should be the one to saturate g5.2xlarge
-  # 15840 available for tasks on g5.xlarge
-  # memory                = 15840 # TODO might need to be adjusted to support bigger jobs/instances.
-  # memory                = 31680 # TODO might need to be adjusted to support bigger jobs/instances.
   memory                = var.processing_ecs_task_memory
   volume {
     name = "data${var.env}"
